07330cd3ab177c4e5e713bb9411c9acb9adab35f
viva-steve <steve@adoc.co.kr>
2021-06-23 00:34:24 +0900

    [FIX] : api url 변경

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
     (async function getData() {
       try {
-        const result = await axios.get("http://localhost/test");
+        const result = await axios.get("http://15.165.252.175/test");
         setData(result.data);
       } catch (err) {
         console.error(err);
